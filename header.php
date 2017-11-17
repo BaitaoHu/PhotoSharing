9908e9d3f6335fa94946dd1664fc4856eb3a2f45
BaitaoHu <hbaotao@163.com>
2017-11-16 23:06:18 -0800

    Modify navbar

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!-- Header included in every PHP file -->
 
 <div id="navbar">
-    <a href="index.php">Home</a><a href="setting.php">Swtting</a>
+    <a href="index.php">Home</a>
 </div>
 Make sure you add your Oracle login info! See the source of this file for details.
